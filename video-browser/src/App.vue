297fe7c1055aa8923d94a0b08e4dfc951e7ea1e2
QXZ1JJS <ThanhDung.Ho@salt-solutions.de>
2021-12-24 00:39:55 +0100

    add axios and make requests with searchterm

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>
-        Hi there!
-    </div>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+const API_KEY = "AIzaSyCXUA_58D6n8sxhwMaE8BeU0rm1f0L4HH8";
+
 export default {
-    name: 'App'
+  name: "App",
+  components: {
+    SearchBar,
+  },
+  methods: {
+    onTermChange: function (searchTerm) {
+      axios.get("https://www.googleapis.com/youtube/v3/search", {
+        params: {
+          key: API_KEY,
+          type: "video",
+          part: "snippet",
+          q: searchTerm
+        },
+      }).then(response => console.log(response));
+    },
+  },
 };
 </script>
